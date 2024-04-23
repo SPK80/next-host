1d833ca4b7e2aab64c419591b8eab6bee7e6004c
psukonkin <psukonkin@phoenixit.ru>
2024-04-23 22:48:07 +0300

    add NextFederationPlugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+const remoteUrl = "https://remote-uikit-8pfo.vercel.app";
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "home",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: { remote: `remote@${remoteUrl}/remoteEntry.js` },
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
